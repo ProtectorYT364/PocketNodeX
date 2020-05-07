967e70945524659c91c1d19e4e6eca4c3597c794
HittmanA <hittmana@comcast.net>
2020-05-06 22:17:23 -0500

    A working, albeit sparse, NBT implementation

--- src/pocketnode/nbt/tags/IntArrayTag.js
+++
@@ -1,9 +0,0 @@
-const Tag = pocketnode("/nbt/tags/Tag.js");
-
-class IntArrayTag extends Tag {
-    constructor(){
-        super(0x0b);
-    }
-}
-
-module.exports = IntArrayTag;
--- src/pocketnode/nbt/tags/LongArrayTag.js
+++
@@ -1,9 +0,0 @@
-const Tag = pocketnode("/nbt/tags/Tag.js");
-
-class LongArrayTag extends Tag {
-    constructor(){
-        super(0x0c);
-    }
-}
-
-module.exports = LongArrayTag;
